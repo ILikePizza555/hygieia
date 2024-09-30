7b9c55bdc3d6a85ecac5c08dc23853843a4cf0f7
Izzy Lancaster <izzy@izzy.sh>
2024-09-29 20:19:40 -0700

    Implemented WasteWaterSample

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::env;
+use std::fmt::format;
+use std::time::{SystemTime, UNIX_EPOCH};
 use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;
 use chrono::{DateTime, Local, MappedLocalTime, NaiveDate, NaiveDateTime, TimeZone};
 use chrono_tz::{Tz, US};
-use futures::{StreamExt, TryStreamExt};
+use color_eyre::eyre;
+use futures::{poll, StreamExt, TryStreamExt};
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 use lambda_runtime::tracing::info;
 use serde::{Deserialize, Deserializer};
 use serde::de::Error as DeserializerError;
 
+fn unix_timestamp_ms() -> i64 {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards: system time is somehow set to before the UNIX epoch.");
+    i64::try_from(now.as_millis())
+        .expect("Current UNIX timestamp is too big for i64. Hello from 2024, creature living in 7849!")
+}
+
+/// Describes a row of data as parsed from the CSV file
 #[derive(Debug, Deserialize)]
 struct WasteWaterCsvRow {
     #[serde(rename = "Sample Collection Date")]
@@ -39,7 +51,54 @@ fn deserialize_pdt_datetime<'de, D>(deserializer: D) -> Result<DateTime<Tz>, D::
         // Clock was turned backwards and now there are two times
         MappedLocalTime::Ambiguous(_, latest) => Ok(latest),
         // Clock was turned forwards and the time doesn't exit
-        MappedLocalTime::None => Err(D::Error::custom(format!("Datetime {} is invalid for Pacific timezone", s)))
+        MappedLocalTime::None => Err(D::Error::custom(format!("Datetime {} is invalid for Pacific timezone.", s)))
+    }
+}
+
+#[derive(Debug)]
+struct WasteWaterSample {
+    /// Semicolon separated concatenation of Site Name, County, PCR Pathogen Target, and the PCR Gene Target
+    sample_summary: String,
+    /// The sample collection date concatenated with the unix timestamp in ms this row was created.
+    sample_collection_sort: String,
+    sample_collection_date: NaiveDate,
+    site_name: String,
+    county: String,
+    pcr_pathogen_target: String,
+    pcr_gene_target: String,
+    normalized_pathogen_concentration: f64,
+    date_updated: DateTime<Tz>,
+    poll_timestamp: i64
+}
+
+impl From<WasteWaterCsvRow> for WasteWaterSample {
+    fn from(row: WasteWaterCsvRow) -> Self {
+        let sample_summary = [
+            row.site_name.as_str(),
+            &row.county,
+            &row.pcr_pathogen_target, 
+            &row.pcr_gene_target].join(";");
+        
+        let poll_timestamp = unix_timestamp_ms();
+        
+        let sample_collection_sort = format!(
+            "{};{}",
+            row.sample_collection_date.format("%Y-%m-%d"),
+            poll_timestamp
+        );
+        
+        Self {
+            sample_summary,
+            sample_collection_sort,
+            sample_collection_date: row.sample_collection_date,
+            site_name: row.site_name,
+            county: row.county,
+            pcr_pathogen_target: row.pcr_pathogen_target,
+            pcr_gene_target: row.pcr_gene_target,
+            normalized_pathogen_concentration: row.normalized_pathogen_concentration,
+            date_updated: row.date_updated,
+            poll_timestamp
+        }
     }
 }
 
@@ -47,10 +106,8 @@ async fn handler(wastewater_url: &str, event: LambdaEvent<CloudWatchEvent>) -> R
     let response = reqwest::get(wastewater_url).await?.error_for_status()?;
     let response_reader = response.bytes_stream().map_err(std::io::Error::other).into_async_read();
     let mut csv_reader = csv_async::AsyncDeserializer::from_reader(response_reader);
-    let mut records = csv_reader.deserialize::<WasteWaterCsvRow>();
-
-    
-
+    let records = csv_reader.deserialize::<WasteWaterCsvRow>();
+    let sample_data = records.map_ok(WasteWaterSample::from);
 
     Ok(())
 }
